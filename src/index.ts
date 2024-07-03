0b7791136cc6c761e2bc7fdf2db4e3449e0fb068
elumixor <vladogim97@gmail.com>
2024-07-03 15:10:41 +0200

    feat: Added keydownonce event handler

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,16 @@ import "./extensions";
 
 export * from "./all";
 export * from "./colors";
+export * from "./default-map";
 export * from "./delay";
 export * from "./event-emitter";
 export * from "./file-data";
 export * from "./get-element-or-throw";
 export * from "./is-mobile";
+export * from "./key-down-once";
 export * from "./non-null";
 export * from "./not-implemented";
 export * from "./random";
 export * from "./types";
-export * from "./zip";
 export * from "./wraps";
-export * from "./default-map";
+export * from "./zip";
---
+++ src/key-down-once.ts
@@ -0,0 +1,75 @@
+const set = new Set();
+const setSecond = new Set();
+
+export const preventKeydownDefault = new (class {
+    conditions: ((e: KeyboardEvent) => boolean)[] = [];
+    strategy: "some" | "every" = "some";
+
+    check(e: KeyboardEvent) {
+        return this.conditions[this.strategy]((c) => c(e));
+    }
+})();
+
+window.addEventListener("blur", () => {
+    set.clear();
+    setSecond.clear();
+});
+
+document.addEventListener(
+    "keydown",
+    (e) => {
+        const { code } = e;
+
+        if (preventKeydownDefault.check(e)) e.preventDefault();
+
+        if (set.has(code)) {
+            if (!setSecond.has(code)) {
+                setSecond.add(code);
+                document.dispatchEvent(new KeyboardEvent("keydownsecond", e));
+            }
+
+            return;
+        }
+
+        set.add(code);
+
+        document.dispatchEvent(new KeyboardEvent("keydownonce", e));
+    },
+    false,
+);
+
+document.addEventListener(
+    "keyup",
+    ({ code }: KeyboardEvent) => {
+        set.delete(code);
+        setSecond.delete(code);
+    },
+    false,
+);
+
+export function isKeyDown(code: string, { secondOnly = false } = {}) {
+    return secondOnly ? setSecond.has(code) : set.has(code);
+}
+
+declare global {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    interface Document {
+        /**
+         * Like "keydown", but will only trigger once when the button is held down
+         */
+        addEventListener(
+            type: "keydownonce",
+            listener: (e: KeyboardEvent) => void,
+            options?: boolean | AddEventListenerOptions,
+        ): void;
+
+        /**
+         * Will fire once when the key is held for some time as a second "keydown" event
+         */
+        addEventListener(
+            type: "keydownsecond",
+            listener: (e: KeyboardEvent) => void,
+            options?: boolean | AddEventListenerOptions,
+        ): void;
+    }
+}
